115e2c1e7d476048fb5a44ef61464959d531e59c
Ved Gupta <vedgupta@protonmail.com>
2022-12-01 19:54:37 +0530

    editior image resize added

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 import ReactQuill, { Quill } from "react-quill";
+import BlotFormatter from "quill-blot-formatter";
 
 import Emoji from "quill-emoji";
 
@@ -10,13 +11,14 @@ import "react-quill/dist/quill.snow.css";
 import "quill-emoji/dist/quill-emoji.css";
 
 Quill.register("modules/emoji", Emoji);
+Quill.register("modules/blotFormatter", BlotFormatter);
 
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, false] }],
   ["bold", "italic", "underline", "strike", "blockquote"],
   [{ list: "ordered" }, { list: "bullet" }],
   [{ indent: "-1" }, { indent: "+1" }],
-  ["link", "image"],
+  ["link", "image", "video"],
   ["emoji"],
   ["clean"],
 ];
@@ -47,8 +49,8 @@ export default function Editor(props: EditorProps) {
         clipboard: {
           matchVisual: false,
         },
+        blotFormatter: {},
         "emoji-toolbar": true,
-        "emoji-textarea": true,
         "emoji-shortname": true,
       }}
       value={value}
